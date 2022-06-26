d2f97a38b605e8731f1ba047367af9d7db5c5654
Henry Faul <rhfaul@gmail.com>
2022-06-26 09:28:13 +0200

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html class="h-full bg-trueGray-100">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- Inertia --}}
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=smoothscroll,NodeList.prototype.forEach,Promise,Object.values,Object.assign" defer></script>
+
+    {{-- Ping CRM --}}
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith" defer></script>
+
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+    @inertiaHead
+</head>
+<body class="font-sans leading-none text-gray-700 antialiased">
+    @inertia
+</body>
+</html>
